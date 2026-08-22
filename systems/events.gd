572e3a2ff1a6f884065274828103f8aac9d15018
feg06 <u202213468@upc.edu.pe>
2026-08-22 17:00:19 -0500

    fix: set camera process callback and ignore unused signal warnings in global events bus

--- systems/events.gd
+++ systems/events.gd
@@ -2,9 +2,15 @@ extends Node
 
 ## Global Events Bus
 
+@warning_ignore("unused_signal")
 signal player_spawned(peer_id: int, player: CharacterBody2D)
+@warning_ignore("unused_signal")
 signal player_despawned(peer_id: int)
+@warning_ignore("unused_signal")
 signal attack_performed(player_id: int, direction: Vector2)
+@warning_ignore("unused_signal")
 signal item_grabbed(item: Node2D, player: CharacterBody2D)
+@warning_ignore("unused_signal")
 signal item_dropped(item: Node2D, position: Vector2)
+@warning_ignore("unused_signal")
 signal room_changed(player_id: int, room_coords: Vector2i)
